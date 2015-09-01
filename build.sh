837f6a54e7a462daee1a0fefbb17b8f21bce7e4d
Thomas Zilliox <thomas@zilliox.me>
2015-09-01 18:23:42 +0200

    Add input-group and addon-group styles

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 function compile() {
   SCSS_FILE="src/"$1;
-  CSS_FILE="build/"$( echo ${1%.*}.css);
-  MIN_CSS_FILE="build/"$( echo ${1%.*}.min.css);
+  CSS_FILE="build/"$2;
+  MIN_CSS_FILE="build/"$( echo ${2%.*}.min.css);
   sass ${SCSS_FILE} ${CSS_FILE} --style expanded --sourcemap=none
   sass ${SCSS_FILE} ${MIN_CSS_FILE} --style compressed --sourcemap=none
 }
 
-compile formidable.scss
+compile formidable.scss formidable.css
+compile themes/lighty.scss formidable-lighty.css
